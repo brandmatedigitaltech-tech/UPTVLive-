d46cc1bd3e1c2cd1a49a94c3a4b89229e00469c7
lets-make-ashwani <ashvish420waniwakarma.com>
2026-05-11 20:03:57 +0530

    improve the error on Tagged News

--- frontend/src/components/ArticlePage/ArticlePage.jsx
+++ frontend/src/components/ArticlePage/ArticlePage.jsx
@@ -1068,7 +1068,7 @@ const ArticlePage = () => {
                         item.slug
                       }
 
-                      to={`/article/${item.slug}`}
+                      to={`/news/${item.slug}`}
 
                       className="related-card"
                     >
